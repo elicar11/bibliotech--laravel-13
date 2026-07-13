832b2523ab8ef115b8f47adea73bc526ea49ab2a
Elicar <anjaracharlinelicar@gmail.com>
2026-07-13 12:43:20 +0300

    Création Model, Controller et Migration Livre

---
+++ app/Models/Livre.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Livre extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'titre',
+        'auteur',
+        'description',
+        'image_couverture',
+        'est_disponible',
+    ];
+}
