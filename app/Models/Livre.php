da715bb46aa13fbe336546997f49789f448f666c
Elicar <anjaracharlinelicar@gmail.com>
2026-07-21 17:03:11 +0300

    Ajout Landing Page

--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -2,18 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+#[Fillable(['titre', 'auteur', 'description', 'image_couverture', 'est_disponible'])] /* Nouveau methode pour version  13 de Mass Assgninment */
 class Livre extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'titre',
-        'auteur',
-        'description',
-        'image_couverture',
-        'est_disponible',
-    ];
+    // Version 12 pour Mass assginment
+    
+    // protected $fillable = [
+    //     'titre',
+    //     'auteur',
+    //     'description',
+    //     'image_couverture',
+    //     'est_disponible',
+    // ];
 }
